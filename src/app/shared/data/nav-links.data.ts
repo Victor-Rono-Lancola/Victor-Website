2a506f46d1a0cf92bd9117644405db4fa8864431
Victor-Rono-Elgia <46953806+Victor-Rono@users.noreply.github.com>
2023-11-01 09:15:48 +0300

    feat: :fire: sidenav completed

--- src/app/shared/data/nav-links.data.ts
+++ src/app/shared/data/nav-links.data.ts
@@ -1,22 +1,23 @@
 import { NavLinkInterface } from "../interfaces/navigation.interface";
 
 export const navLinksData: NavLinkInterface[] = [
+  // {
+  //   title: 'Home',
+  //   url: '',
+  // },
   {
-    title: 'Home',
-    url: '',
+    title: 'My Github',
+    url: 'https://github.com/Victor-Rono',
+    icon: 'fab fa-github',
   },
-  {
-    title: 'About Me',
-    url: 'about',
-  },
-  {
-    title: 'Contact',
-    url: 'contact',
-  },
-  {
-    title: 'Login',
-    url: 'login',
-    button: true,
-    icon: 'fa fa-user',
-  }
+  // {
+  //   title: 'Contact',
+  //   url: 'contact',
+  // },
+  // {
+  //   title: 'Login',
+  //   url: 'login',
+  //   button: true,
+  //   icon: 'fa fa-user',
+  // }
 ]
